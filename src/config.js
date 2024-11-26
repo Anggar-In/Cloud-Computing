0eee4e7242a1ea8a82b99d295707ef3c06ce6979
Whilyan Pratama <pratamawhilyan399@gmail.com>
2024-11-27 02:59:23 +0700

    add ocr api

--- src/config.js
+++ src/config.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-require('dotenv').config(); 
-=======
-require("dotenv").config();
->>>>>>> f186631f2b34b20dafb1ea9ceee0cc95dc1e2931
-
 module.exports = {
   SECRET_KEY: process.env.SECRET_KEY,
   DB_HOST: process.env.DB_HOST,
