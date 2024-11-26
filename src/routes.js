cffd4091bac1fcd50c36d6e80f8a912d65be91ec
Whilyan Pratama <pratamawhilyan399@gmail.com>
2024-11-27 03:02:59 +0700

    add ocr api

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const router = express.Router();
 const { register, login, postTransaction, postReceipt } = require('./controller'); 
@@ -11,16 +10,5 @@ router.post('/auth/register', register);
 router.post('/auth/login', login);
 router.post('/transaction', postTransaction);
 router.post('/upload-receipt', upload.single('receipt'), postReceipt);
-=======
-const express = require("express");
-const router = express.Router();
-const { register, login, Transaction } = require("./controller");
-
-router.post("/auth/register", register);
-
-router.post("/auth/login", login);
-
-router.post("/transaction", Transaction);
->>>>>>> f186631f2b34b20dafb1ea9ceee0cc95dc1e2931
 
 module.exports = router;
