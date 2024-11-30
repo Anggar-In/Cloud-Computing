4905227666c731cf8f8b1b63674d4d952262a197
Whilyan Pratama <pratamawhilyan399@gmail.com>
2024-12-01 03:12:06 +0700

    fix login api, register & add budgeting api + expense api

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, postTransaction, postReceipt } = require('./controller'); 
+const { register, 
+  login, 
+  postTransaction, 
+  postReceipt, 
+  postBudget, 
+  getBudget, 
+  putBudget, 
+  deleteBudget, 
+  logout, 
+  postExpense, 
+  getExpense, 
+  putExpense, 
+  deleteExpense } = require('./controller'); 
 
 const multer = require('multer');
 const storage = multer.memoryStorage(); 
@@ -10,5 +22,17 @@ router.post('/auth/register', register);
 router.post('/auth/login', login);
 router.post('/transaction', postTransaction);
 router.post('/upload-receipt', upload.single('receipt'), postReceipt);
+router.post('/budget', postBudget);
+router.post('/auth/logout', logout);
+router.post('/expense', postExpense); 
+
+router.get('/budgets/:user_id', getBudget);
+router.get('/expenses/:user_id', getExpense); 
+
+router.put('/budget/:budget_id', putBudget); 
+router.put('/expense/:expense_id', putExpense);
+
+router.delete('/budget/:budget_id', deleteBudget); 
+router.delete('/expense/:expense_id', deleteExpense);
 
 module.exports = router;
