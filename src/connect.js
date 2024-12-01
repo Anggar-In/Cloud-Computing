f7b10b9e1fdb3b043b7f8b573049415e5fdc7712
Muhammad Alvanditya Sasongko <118495007+vandsas@users.noreply.github.com>
2024-12-01 21:50:40 +0700

    Update connect.js without async
    update to no async

--- src/connect.js
+++ src/connect.js
@@ -1,22 +1,19 @@
-const mysql = require('mysql2/promise');
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = require('./config');
+const mysql = require("mysql2");
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = require("./config");
+const db = mysql.createConnection({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  port: 3306,
+});
 
-async function connectDB() {
-  try {
-    const db = await mysql.createConnection({
-      host: DB_HOST,
-      user: DB_USER,
-      password: DB_PASSWORD,
-      database: DB_NAME,
-    });
-
-    console.log("Connected to MySQL database");
-
-    return db; 
-  } catch (err) {
+db.connect((err) => {
+  if (err) {
     console.error("Error connecting to the database:", err);
-    throw err;
+    return;
   }
-}
+  console.log("Connected to MySQL database");
+});
 
-module.exports = connectDB;
+module.exports = db;
