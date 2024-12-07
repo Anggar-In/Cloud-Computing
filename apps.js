8e43f35a840fe74489c39390ce4cd0d45af8ecac
Whilyan Pratama <pratamawhilyan399@gmail.com>
2024-12-07 22:19:04 +0700

    add swagger

--- apps.js
+++ apps.js
@@ -3,7 +3,13 @@ const bodyParser = require("body-parser");
 const logRequest = require('./src/log');
 const app = express();
 const routes = require('./src/routes');
+const swaggerUi = require('swagger-ui-express');
+const fs = require('fs');
+const yaml = require('js-yaml');
 
+
+const swaggerDocument = yaml.load(fs.readFileSync('./api_test/swagger.yaml', 'utf8'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json()); 
 app.use(routes); 
 app.use(bodyParser.json());
